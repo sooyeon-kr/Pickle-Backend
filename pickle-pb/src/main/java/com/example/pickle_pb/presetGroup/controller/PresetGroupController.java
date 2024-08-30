3783d99c233ea08028530dd662e8ba7b1d78224c
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-08-30 09:53:59 +0900

    feat: presetGroup 생성 기능 추가

--- pickle-pb/src/main/java/com/example/pickle_pb/presetGroup/controller/PresetGroupController.java
+++ pickle-pb/src/main/java/com/example/pickle_pb/presetGroup/controller/PresetGroupController.java
@@ -1,16 +1,13 @@
 package com.example.pickle_pb.presetGroup.controller;
 
-import com.example.pickle_pb.presetGroup.dto.PresetGroupResponseDto;
+import com.example.pickle_pb.presetGroup.dto.PresetGroupRequestDto.*;
 import com.example.pickle_pb.presetGroup.service.PresetGroupService;
 import com.example.real_common.global.common.CommonResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.example.pickle_pb.presetGroup.dto.PresetGroupResponseDto.*;
 
@@ -25,5 +22,11 @@ public class PresetGroupController {
     public ResponseEntity<CommonResDto<?>> readPresetGroup() {
         ReadPresetGroupResponseDto result = presetGroupService.readPresetGroup();
         return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "프리셋 그룹 조회 성공", result));
+    }
+
+    @PostMapping
+    public ResponseEntity<CommonResDto<?>> createPresetGroup(CreatePresetGroupRequestDto requestDto) {
+        CreatePresetGroupResponseDto result = presetGroupService.createPresetGroup(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResDto<>(1, "프리셋 생성 성공", result));
     }
 }
