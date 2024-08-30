ff16017d9fbeb166fee9c462ad6fd01151e94403
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-08-30 09:54:09 +0900

    feat: 에러처리 추가

--- real-common/src/main/java/com/example/real_common/global/exception/ErrorCode.java
+++ real-common/src/main/java/com/example/real_common/global/exception/ErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    CUSTOM_TEST_EXCEPTION(HttpStatus.BAD_REQUEST, "TEST_001", "에러 핸들러 테스트입니다.");
+    CUSTOM_TEST_EXCEPTION(HttpStatus.BAD_REQUEST, "TEST_001", "에러 핸들러 테스트입니다."),
+    NOT_FOUND_ACCOUNT_EXCEPTION(HttpStatus.UNAUTHORIZED, "ACCOUNT_001","등록된 계정이 없음");
 
     private final String code;
     private final String message;
