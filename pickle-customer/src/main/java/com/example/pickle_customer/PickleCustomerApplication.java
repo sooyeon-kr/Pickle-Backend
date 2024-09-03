1dac615a9b646a94dbc73709fbdfe64d4c23c50f
sooyeon-kr <sooyeon7027@naver.com>
2024-09-03 10:25:38 +0900

    fix: 누락된 공통 모듈 사용에 필요한 설정 추가 및 기존 Entity 이름 변경 시 누락됐던 부분들 이름 추가적으로 변경

--- pickle-customer/src/main/java/com/example/pickle_customer/PickleCustomerApplication.java
+++ pickle-customer/src/main/java/com/example/pickle_customer/PickleCustomerApplication.java
@@ -2,9 +2,10 @@ package com.example.pickle_customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.pickle_customer", "com.example.real_common"})
+@EnableJpaAuditing
 public class PickleCustomerApplication {
 
 	public static void main(String[] args) {
