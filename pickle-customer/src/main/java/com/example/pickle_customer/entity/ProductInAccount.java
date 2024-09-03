017b94e969baf38276c88d206867d42ce9f0ef0e
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-03 11:43:48 +0900

    ✨feat: annotation 추가

--- pickle-customer/src/main/java/com/example/pickle_customer/entity/ProductInAccount.java
+++ pickle-customer/src/main/java/com/example/pickle_customer/entity/ProductInAccount.java
@@ -6,10 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductInAccount {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
