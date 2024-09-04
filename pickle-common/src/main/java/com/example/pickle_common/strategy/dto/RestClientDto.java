6c38ab37c0f83eb3c4ce5049439bd9ce4c845efe
kim nahyeon <stome6322@inha.edu>
2024-09-04 16:06:02 +0900

    feat: 내부 통신용 전략 조회 /inner/strategy/{strategyId} 구현

--- pickle-common/src/main/java/com/example/pickle_common/strategy/dto/RestClientDto.java
+++ pickle-common/src/main/java/com/example/pickle_common/strategy/dto/RestClientDto.java
@@ -1,7 +1,12 @@
 package com.example.pickle_common.strategy.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 public class RestClientDto {
 
@@ -11,5 +16,15 @@ public class RestClientDto {
         private String name;
         @NotBlank
         private String branchOffice;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ReadStrategyResponseDto {
+        private String name;
+        private int strategyId;
+        private List<ReadDetailStrategyResponseDto.CategoryDto> categoryList;
     }
 }
