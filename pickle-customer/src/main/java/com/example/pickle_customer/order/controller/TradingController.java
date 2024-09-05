5eaeadbda44d38f018bea587b1851cdf5be9886b
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-05 17:02:35 +0900

    ✨feat: 예외처리,토큰으로부터 accountId가져오기 로직 추가

--- pickle-customer/src/main/java/com/example/pickle_customer/order/controller/TradingController.java
+++ pickle-customer/src/main/java/com/example/pickle_customer/order/controller/TradingController.java
@@ -1,5 +1,7 @@
 package com.example.pickle_customer.order.controller;
 
+import com.example.pickle_customer.auth.JwtService;
+import com.example.pickle_customer.dto.AccountResponseDto;
 import com.example.pickle_customer.mystrategy.dto.CreateMyStrategyDto;
 import com.example.pickle_customer.mystrategy.service.MyStrategyService;
 import com.example.pickle_customer.order.dto.ProductInAccountSaveDTO;
@@ -7,40 +9,50 @@ import com.example.pickle_customer.order.dto.TradingRequestDTO;
 import com.example.pickle_customer.order.dto.UpdateTotalAmountDTO;
 import com.example.pickle_customer.order.service.TradingService;
 import com.example.pickle_customer.repository.ProductRepository;
+import com.example.pickle_customer.service.AccountService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pickle-customer/api/trade")
 @AllArgsConstructor
 public class TradingController {
     private final ProductRepository productRepository;
+    private final JwtService jwtService;
     private TradingService tradingService;
     private MyStrategyService myStrategyService;
+    private AccountService accountService;
     @PostMapping(value = "/")
-    public  ResponseEntity<String> trading(@RequestBody TradingRequestDTO tradingRequestDTO){
-        CreateMyStrategyDto.Request strategyRequest =CreateMyStrategyDto.Request.builder()
-                        .accountId(1)
-                        .selectedStrategyId(tradingRequestDTO.getStrategyId())
-                        .build();
-        CreateMyStrategyDto.Response strategyResponse=myStrategyService.createMyStrategy(strategyRequest);
-        UpdateTotalAmountDTO updateRequest=UpdateTotalAmountDTO.builder()
-                .accountId(1)
-                .tradingRequestDTO(tradingRequestDTO)
-                .build();
-        tradingService.updateTotalAmount(updateRequest);
+    public  ResponseEntity<String> trading(@RequestBody TradingRequestDTO tradingRequestDTO
+    ,@RequestHeader(HttpHeaders.AUTHORIZATION) String token){
+        try {
+            AccountResponseDto accountResponseDto = accountService.myAsset(token);
+            int accountId = accountResponseDto.getAccountId();
+            CreateMyStrategyDto.Request strategyRequest = CreateMyStrategyDto.Request.builder()
+                    .accountId(accountId)
+                    .selectedStrategyId(tradingRequestDTO.getStrategyId())
+                    .build();
+            CreateMyStrategyDto.Response strategyResponse = myStrategyService.createMyStrategy(strategyRequest);
+            UpdateTotalAmountDTO updateRequest = UpdateTotalAmountDTO.builder()
+                    .accountId(accountId)
+                    .tradingRequestDTO(tradingRequestDTO)
+                    .build();
+            tradingService.updateTotalAmount(updateRequest);
 
-        ProductInAccountSaveDTO productInAccountSaveDTO=ProductInAccountSaveDTO.builder()
-                .strategyId(strategyResponse.getCreatedMyStrategyId())
-                .tradingRequestDTO(tradingRequestDTO)
-                .build();
-        tradingService.productInAccountSave(productInAccountSaveDTO);
-        return ResponseEntity.ok("Trading operation completed successfully");
+            ProductInAccountSaveDTO productInAccountSaveDTO = ProductInAccountSaveDTO.builder()
+                    .strategyId(strategyResponse.getCreatedMyStrategyId())
+                    .tradingRequestDTO(tradingRequestDTO)
+                    .build();
+            tradingService.productInAccountSave(productInAccountSaveDTO);
+            return ResponseEntity.ok("Trading completed successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error processing trading");
+        }
     }
+
 
 
 }
