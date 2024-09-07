b27066e48c318bdf9a710e7d09c4a46cf1715901
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-07 21:59:30 +0900

    🐛fix: controller 에러 수정

--- pickle-common/src/main/java/com/example/pickle_common/strategy/controller/InnerStrategyController.java
+++ pickle-common/src/main/java/com/example/pickle_common/strategy/controller/InnerStrategyController.java
@@ -5,10 +5,8 @@ import com.example.pickle_common.strategy.service.StrategyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +16,10 @@ public class InnerStrategyController {
     private final StrategyService strategyService;
 
     @GetMapping("/{strategyId}")
-    public ResponseEntity<?> getStrategy(@PathVariable int strategyId) {
+    public ResponseEntity<?> getStrategy(@PathVariable("strategyId") int strategyId) {
         RestClientDto.ReadStrategyResponseDto result = strategyService.getStrategy(strategyId);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
+
+
 }
