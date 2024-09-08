4b3bf1db1fd8617da71528ebdbb6cb485469b10d
kim nahyeon <stome6322@inha.edu>
2024-09-08 13:27:11 +0900

    feat: 전략 생성, 세부 조회 시 사용자 인증 로직 추가

--- pickle-common/src/main/java/com/example/pickle_common/strategy/controller/StrategyController.java
+++ pickle-common/src/main/java/com/example/pickle_common/strategy/controller/StrategyController.java
@@ -24,26 +24,26 @@ public class StrategyController {
     private final StrategyService strategyService;
 
     @PostMapping
-    public ResponseEntity<CommonResDto<?>> createStrategy(@RequestBody @Valid CreateStrategyRequestDto createStrategyRequestDto) {
-        CreateStrategyResponseDto responseDto = strategyService.postStrategy(createStrategyRequestDto);
+    public ResponseEntity<CommonResDto<?>> createStrategy(@RequestHeader("Authorization") String authorizationHeader, @RequestBody @Valid CreateStrategyRequestDto createStrategyRequestDto) {
+        CreateStrategyResponseDto responseDto = strategyService.postStrategy(createStrategyRequestDto, authorizationHeader);
         return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResDto<>(1, "전략 생성 성공", responseDto));
     }
 
     @GetMapping
-    public ResponseEntity<CommonResDto<?>> readStrategy() {
-        ReadStrategyResponseDto result = strategyService.readStrategy();
+    public ResponseEntity<CommonResDto<?>> readStrategy(@RequestHeader("Authorization") String authorizationHeader) {
+        ReadStrategyResponseDto result = strategyService.readStrategy(authorizationHeader);
         return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "전략 조회 성공", result));
     }
 
     @GetMapping("/pb/{strategyId}")
-    public ResponseEntity<CommonResDto<?>> pbReadDetailStrategy(@PathVariable("strategyId") Integer strategyId) {
-        ReadDetailStrategyResponseDto result = strategyService.pbReadDetailStrategy(strategyId);
+    public ResponseEntity<CommonResDto<?>> pbReadDetailStrategy(@PathVariable("strategyId") Integer strategyId, @RequestHeader("Authorization") String authorizationHeader) {
+        ReadDetailStrategyResponseDto result = strategyService.pbReadDetailStrategy(strategyId, authorizationHeader);
         return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "전략 상세조회 성공", result));
     }
 
     @GetMapping("/customer/{strategyId}")
-    public ResponseEntity<CommonResDto<?>> cusReadDetailStrategy(@PathVariable("strategyId") Integer strategyId) {
-        ReadDetailStrategyResponseDto result = strategyService.cusReadDetailStrategy(strategyId);
+    public ResponseEntity<CommonResDto<?>> cusReadDetailStrategy(@PathVariable("strategyId") Integer strategyId, @RequestHeader("Authorization") String authorizationHeader) {
+        ReadDetailStrategyResponseDto result = strategyService.cusReadDetailStrategy(strategyId, authorizationHeader);
         return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "전략 상세조회 성공", result));
     }
 }
