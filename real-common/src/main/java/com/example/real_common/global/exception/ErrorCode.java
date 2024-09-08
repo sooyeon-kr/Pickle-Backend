987e4d36d66f8465699de25d775657958e56f909
sooyeon-kr <sooyeon7027@naver.com>
2024-09-08 16:02:57 +0900

    feat: 예외코드 추가 작성

--- real-common/src/main/java/com/example/real_common/global/exception/ErrorCode.java
+++ real-common/src/main/java/com/example/real_common/global/exception/ErrorCode.java
@@ -16,9 +16,13 @@ public enum ErrorCode {
     UN_AUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "ACCOUNT_002", "사용자의 권한이 아님"),
     NOT_FOUND_STRATEGY_EXCEPTION(HttpStatus.NOT_FOUND, "STRATEGY_001", "전략을 찾을 수 없음"),
     NOT_FOUND_MY_STRATEGY_EXCEPTION(HttpStatus.NOT_FOUND, "STRATEGY_001", "나의 전략을 찾을 수 없음"),
-
     CONFLICT_MY_STRATEGY_EXCEPTION(HttpStatus.CONFLICT, "MY_STRATEGY_002", "이미 나의 전략이 존재함"),
-    NOT_FOUND_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "IMAGE_001", "사진이 없습니다.");
+    NOT_FOUND_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "IMAGE_001", "사진이 없습니다."),
+    UNABLE_TO_CREATE_REQUEST_LETTER_EXCEPTION(HttpStatus.BAD_REQUEST, "CONSULTING_002", "요청서를 생성할 수 없음"),
+    NOT_FOUND_REQUEST_LETTER_EXCEPTION(HttpStatus.NOT_FOUND, "CONSULTING_003", "요청서를 찾을 수 없음"),
+    UNABLE_TO_CREATE_REQUEST_LETTER_DUE_TO_MQ_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "CONSULTING_004", "내부 통신 에러로 인해, "),
+    UNEXPECTED_SERVICE_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_001", "예기치 않은 예외");
+
     private final String code;
     private final String message;
     private final HttpStatus status;
